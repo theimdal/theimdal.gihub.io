951b75aa386005f49fe4ffa12b90b3f299bfe030
Tord Stenseth Heimdal <toh@dips.no>
2018-04-28 22:16:43 +0200

    First take on a plain javascript router

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const r = new Router(
+    {
+      about: new Layout(new Page('menu.html'), new Page('about.html')),
+      home: new Layout(new Page('menu.html'), new Page('home.html')),
+      links: new Layout(new Page('menu.html'), new Page('links.html')),
+      '#default': new Page('home.html'),
+    },
+    document.querySelector('main')
+  );
+  
---
+++ js/page.js
@@ -0,0 +1,14 @@
+class Page {
+    constructor(url) {
+      this.url = 'views/' + url;
+    }
+  
+    load() {
+      return $.get(this.url).then(res => this.html = res);
+    }  
+  
+    show(el) {
+      el.innerHTML = this.html;
+    }
+  }
+  
