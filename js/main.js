aea4fc7aa841ed4413cab68f776176c285e42493
Tord Stenseth Heimdal <toh@dips.no>
2018-04-29 09:45:24 +0200

    Added layout to default route

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const r = new Router(
       about: new Layout(new Page('menu.html'), new Page('about.html')),
       home: new Layout(new Page('menu.html'), new Page('home.html')),
       links: new Layout(new Page('menu.html'), new Page('links.html')),
-      '#default': new Page('home.html'),
+      '#default': new Layout(new Page('menu.html'), new Page('home.html')),
     },
     document.querySelector('main')
   );
